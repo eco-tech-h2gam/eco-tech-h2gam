9e936f3065fd304240d37bbd52c5f171a2177ec9
Ludovic Giraud <ludovic.giraud@essec.edu>
2024-01-02 03:14:34 +0100

    tidied up

--- maps.py
+++ maps.py
@@ -541,14 +541,11 @@ class compute_maps:
             file_path = os.path.join(folder, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
-                    send2trash.send2trash(file_path)
-                    print(f"Moved to recycle bin: {file_path}")
+                    os.unlink(file_path)
                 elif os.path.isdir(file_path):
                     shutil.rmtree(file_path)
-                    print(f"Deleted: {file_path}")
             except Exception as e:
-                print(f'Failed to delete {file_path}. Reason: {e}')
-        
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
         gifName = 'PM2.5_concentration-{:}.gif'.format(currentDatestring)
         if sys == "Windows":
             duration = 1  # Adjust the duration value as needed
