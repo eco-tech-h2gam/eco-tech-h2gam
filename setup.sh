2789e8a6b7bb3af93f78e9c74efe8e80eb03705d
Ludovic Giraud <ludovic.giraud@essec.edu>
2024-01-06 13:12:33 +0100

    modified setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,30 +1,3 @@
-#!/bin/bash
-
-# Set UTF-8 encoding
-export PYTHONUTF8=1
-
-# Install Miniconda (replace with the appropriate URL for your platform)
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-rm miniconda.sh
-
-# Update conda and create a Conda environment
-export PATH="$HOME/miniconda/bin:$PATH"
-conda update -n base -c defaults conda
-conda env create -f environment.yml
-
-# Activate the Conda environment
-source $HOME/miniconda/etc/profile.d/conda.sh
-conda activate eco-tech-h2gam
-
-# Install your Conda dependencies
-conda install --yes --file environment.yml
-
-# (Optional) Install additional Python dependencies using pip
-pip install -r requirements.txt
-
-# Your other setup commands go here
-
 # Create Streamlit config files
 mkdir -p ~/.streamlit/
 
